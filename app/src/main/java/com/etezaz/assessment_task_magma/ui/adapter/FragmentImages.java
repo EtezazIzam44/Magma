37ea6cc4fa25ae296fb6f9d655bc8cec480d0f0c
Etezaz <etezazizam44@gmail.com>
2020-10-29 23:39:36 +0300

    Adding Firebase

--- app/src/main/java/com/etezaz/assessment_task_magma/ui/adapter/FragmentImages.java
+++ app/src/main/java/com/etezaz/assessment_task_magma/ui/adapter/FragmentImages.java
@@ -1,6 +1,7 @@
 package com.etezaz.assessment_task_magma.ui.adapter;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,7 +12,13 @@ import com.etezaz.assessment_task_magma.model.db.table.BhAdsImageStatus;
 import com.etezaz.assessment_task_magma.presenter.BhAdsPresenter;
 import com.etezaz.assessment_task_magma.view.BhAdsView;
 import com.etezaz.assessment_task_magma.view.OnItemClickListener;
+import com.etezaz.assessment_task_magma.zoom.FragmentZoom;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 import androidx.fragment.app.Fragment;
@@ -43,6 +50,34 @@ public class FragmentImages extends Fragment implements BhAdsView {
         imagesPresenter = new BhAdsPresenter(this, getContext());
 
         imagesPresenter.getAllBhAdsImageStatus();
+
+        //region Firebase
+        //mykey.json you get from FBconsole/Project Settings/service accounts/generte new private key
+
+        File myFile = new File("2baOEUTpE2/M7El0gEKt+1GfSn4qD+c8SMBtFm6g");
+
+        GoogleCredential googleCred = null;
+        try {
+            googleCred = GoogleCredential.fromStream(new FileInputStream(myFile));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        GoogleCredential scoped = googleCred.createScoped(
+                Arrays.asList(
+                        "https://www.googleapis.com/auth/firebase.database",
+                        "https://www.googleapis.com/auth/userinfo.email"
+                )
+        );
+        try {
+            scoped.refreshToken();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String token = scoped.getAccessToken();
+        Log.d("token ", token);
+
+
+        //endregion
 
         return view;
     }
