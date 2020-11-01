6ebf82bfc0f74b85f2cf39197d290d95d2dec6de
Etezaz <etezazizam44@gmail.com>
2020-11-01 16:43:17 +0200

    Cleaning

--- app/src/main/java/com/etezaz/assessment_task_magma/MainActivity.java
+++ app/src/main/java/com/etezaz/assessment_task_magma/MainActivity.java
@@ -23,60 +23,7 @@ public class MainActivity extends AppCompatActivity {
                 .replace(R.id.linear_layout, new FragmentImages())
                 .commit();
 
-     /*   try {
-            //MongoClient mongoClient = new MongoClient("10.1.1.1", 27017);//new MongoClient("mongodb://10.1.1.1:27017");
-            MongoClient mongoClient = new MongoClient("mongodb://35.222.197.123", 47692);
-            //String dbURI = "mongodb://35.222.197.123:27017";
-           // MongoClient mongoClient = new MongoClient(new MongoClientURI(dbURI));
-
-            DB db = mongoClient.getDB("waseet_data");
-            Log.d("LOG_TAG", "DBName = " + db.getName());
-
-            Set<String> collections = db.getCollectionNames();
-            for (String colName : collections) {
-                Log.d("LOG_TAG", "colName = " + colName);
-                System.out.println("\t + Collection: " + colName);
-            }
-
-            mongoClient.close();
-        }
-        catch (Exception e){
-            e.printStackTrace();
-            Log.d("LOG_TAG", "exceptionMongo = " + e);
-        }
-    }*/
     }
 
-  /*  public void listAllPaginated(@Nullable String pageToken) {
-        FirebaseStorage storage = FirebaseStorage.getInstance();
-        StorageReference listRef = storage.getReference().child("files/uid");
 
-        // Fetch the next page of results, using the pageToken if we have one.
-        Task<ListResult> listPageTask = pageToken != null
-                ? listRef.list(100, pageToken)
-                : listRef.list(100);
-
-        listPageTask
-                .addOnSuccessListener(new OnSuccessListener<ListResult>() {
-                    @Override
-                    public void onSuccess(ListResult listResult) {
-                        List<StorageReference> prefixes = listResult.getPrefixes();
-                        List<StorageReference> items = listResult.getItems();
-
-                        // Process page of results
-                        // ...
-
-                        // Recurse onto next page
-                        if (listResult.getPageToken() != null) {
-                            listAllPaginated(listResult.getPageToken());
-                        }
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                // Uh-oh, an error occurred.
-            }
-        });
-    }
-*/
 }
